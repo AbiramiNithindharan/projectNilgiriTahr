2ed616aece71712d6fa6d65f598ad06fff93057f
AbiramiNithindharan <abiramimuthu7@gmail.com>
2025-10-04 17:19:11 +0530

    update: deleted routes api temporarily

--- src/app/api/auth/logout/route.js
+++
@@ -1,8 +0,0 @@
-import { clearAuthCookie } from "@/utils/auth";
-
-export async function POST() {
-  clearAuthCookie();
-  return new Response(JSON.stringify({ message: "Logged out" }), {
-    status: 200,
-  });
-}
