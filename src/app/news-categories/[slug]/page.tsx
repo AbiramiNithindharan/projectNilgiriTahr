47583e765db85fcac78228970294cb91b87767f0
AbiramiNithindharan <abiramimuthu7@gmail.com>
2025-10-16 10:44:29 +0530

    update: clear the error issue

--- src/app/news-categories/[slug]/page.tsx
+++ src/app/news-categories/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { groq } from "next-sanity";
-import client from "@/lib/sanityClient";
+import { client } from "@/lib/sanityClient";
 import CategoryClient from "./categoryClient";
 
 export const revalidate = 60;
