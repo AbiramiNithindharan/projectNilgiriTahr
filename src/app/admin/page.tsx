438cfe2d9e51062f008e0b5ad7c8de0e32c009be
AbiramiNithindharan <abiramimuthu7@gmail.com>
2026-04-11 11:29:48 +0530

    Added Suspense in /admin route

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import AdminClient from "./AdminClient";
 
 export default function CMSPortal() {
-  return <AdminClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AdminClient />;
+    </Suspense>
+  );
 }
